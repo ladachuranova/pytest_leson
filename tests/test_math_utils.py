243409a62df897c1df0474f29211dc1a8531e9ec
lyalya <lalachuranova@gmail.com->
2025-09-18 15:07:56 +0300

    add params tests

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,11 +1,24 @@
+import pytest
+
 from src.math_utils import multiply
+
+
 
 
 def test_basic_math():
     assert  2 + 2 == 4
 
-def test_multiply():
-    assert multiply(3, 5) == 15, "функция загрушена не правильно"
 
-def test_multiple_negative():
-    assert multiply(3, 5) == 15, "функция загрушена не правильно"
+@pytest.mark.parametrize(
+    "num1, num2, result",
+    [
+    (2, 2, 4),
+    (3, 5, 15),
+    (-3, 5, -15),
+    (-3, -5, 15)
+    ]
+)
+def test_multiply(num1, num2, result):
+    assert multiply(num1, num2)== result
+    "функция загрушена не правильно"
+
